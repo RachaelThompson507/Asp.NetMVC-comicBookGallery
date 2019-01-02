e21df06b589cad8a148c22f7ced340bade41d521
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2019-01-02 17:06:08 -0600

    added comicbook detail view

--- src/ComicBookGallery/Controllers/comicBooksController.cs
+++ src/ComicBookGallery/Controllers/comicBooksController.cs
@@ -11,14 +11,17 @@ namespace ComicBookGallery.Controllers
         //Action result is the base class for most actions in a Controller base class
         public ActionResult Detail()
         {
-            //Date Time practice stuff
+            return View();
+            /*
+            Date Time practice stuff
             if (DateTime.Today.DayOfWeek==DayOfWeek.Monday)
             {
-                //Redirect is a method of Action Result Class
+                Redirect is a method of Action Result Class
                 return  Redirect("/");
             }
-            //Content is a method of Action Result Class
+            Content is a method of Action Result Class
             return Content("Hello, From the Comic Books Controller!");
+            */
         }
     }
 }

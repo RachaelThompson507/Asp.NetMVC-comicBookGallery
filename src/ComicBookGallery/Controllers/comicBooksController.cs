4c3283376342e5a3349e3b0dbe4486385d1baf9c
Rachael Thompson <rachael.elizabeth.thompson@gmail.com>
2019-01-05 17:50:37 -0600

    moved data to controller

--- src/ComicBookGallery/Controllers/comicBooksController.cs
+++ src/ComicBookGallery/Controllers/comicBooksController.cs
@@ -11,6 +11,17 @@ namespace ComicBookGallery.Controllers
         //Action result is the base class for most actions in a Controller base class
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
             return View();
             /*
             Date Time practice stuff
